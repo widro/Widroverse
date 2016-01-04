fb21f9fa9cee254b7628a8fe8aba7a42883809e2
Jonathan Widro <jwidro@gmail.com>
2016-01-04 03:30:06 -0500

    db update

--- ticks/config/dbconnect.php
+++ ticks/config/dbconnect.php
@@ -2,7 +2,8 @@
 $thisurl =  $_SERVER['HTTP_HOST'];
 
 if(($thisurl=="www.widroverse.com")||($thisurl=="widroverse.com")){
-	$connection = mysql_connect( $_ENV{DATABASE_SERVER}, "db54729", "O47jhg(%vfoyh") or die("Couldn't connect.");
+	$dbname = "db145717_widroverse";
+	$connection = mysql_connect('internal-db.s145717.gridserver.com', "db145717", "8uhb8uhb") or die("Couldn't connect.");
 	$db = mysql_select_db($dbname) or die("Couldn't select database");
 }
 else{
