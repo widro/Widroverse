6513750fa59e21213d0ac2e720cc6b0ab610f395
Jonathan Widro <jwidro@gmail.com>
2016-01-10 04:55:17 -0500

    components folder and initial components

---
+++ ticks/components/menu.php
@@ -0,0 +1,77 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">TickAdmin</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li><a href="#">TickStats</a></li>
+        <li><a href="#">Checklists</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Life</a></li>
+            <li><a href="#">Food</a></li>
+            <li><a href="#">Cooking</a></li>
+            <li><a href="#">Exercise</a></li>
+            <li><a href="#">Inventory</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Video Games</a></li>
+            <li><a href="#">Books</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">Music</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Inside Pulse</a></li>
+            <li><a href="#">GameCo</a></li>
+            <li><a href="#">PANOS Brands</a></li>
+            <li><a href="#">Digital Grout</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Widro.com</a></li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="inventory.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Inventory <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="inventory.php">Mugs</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Scavenger Hunt</a></li>
+        <li><a href="#">FAQ</a></li>
+      </ul>
+      <!--
+      <form class="navbar-form navbar-left" role="search">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+      </form>
+      -->
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">Widroverse Home</a></li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Tools <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="all.php">All</a></li>
+            <li><a href="generic.php">Generic</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Excel Paste</a></li>
+            <li><a href="#">Fix Up Tick Data</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Schedule Items</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
---
+++ ticks/components/tickstream.php
@@ -0,0 +1,10 @@
+		<div class="col-lg-3 tickstream">
+			<h2>Tickstream</h2>
+			<div class="col-lg-12">
+				<a href="#" id="">Latest</a> | 
+				<a href="#" id="">Yesterday</a> | 
+				<a href="#" id="">Life Only</a> | 
+				<a href="#" id="">Today Only</a>
+			</div>
+			<div id="tickstream_content" class="col-lg-12"></div>
+		</div>
