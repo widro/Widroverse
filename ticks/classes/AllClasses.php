bdc965d10e46c84aaeb3a8242d99388ff0a969bd
Jonathan Widro <jwidro@gmail.com>
2016-01-26 05:03:41 -0500

    contacts class

--- ticks/classes/AllClasses.php
+++ ticks/classes/AllClasses.php
@@ -4,6 +4,7 @@ include('config/dbconnect.php');
 
 include('classes/Book.php');
 include('classes/Category.php');
+include('classes/Contact.php');
 include('classes/Game.php');
 include('classes/Ingredient.php');
 include('classes/Inventory.php');
