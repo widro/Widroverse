113d8345544809962ae18c3499b0701897941eff
Jonathan Widro <jwidro@gmail.com>
2016-03-02 18:41:17 -0500

    v12 php converted to v12 folder

---
+++ ticks/v12/footer.php
@@ -0,0 +1,43 @@
+	</div>
+	<div class="col-lg-3">
+		<div class="rightsidebar poll">
+			<h2>Vote Now</h2>
+			<p>Content here</p>
+		</div>
+
+		<div class="rightsidebar raw">
+			<h2>Monday Night Raw</h2>
+			<p>Content here</p>
+		</div>
+
+		<div class="rightsidebar tna">
+			<h2>TNA Impact</h2>
+			<p>Content here</p>
+		</div>
+
+		<div class="rightsidebar luchaunderground">
+			<h2>Lucha Underground</h2>
+			<p>Content here</p>
+		</div>
+
+		<div class="rightsidebar smackdown">
+			<h2>Thursday Night Smackdown</h2>
+			<p>Content here</p>
+		</div>
+
+		<div class="rightsidebar titlehistory">
+			<h2>Title History</h2>
+			<p>Content here</p>
+		</div>
+
+
+
+
+	</div>
+</div>
+
+
+
+
+</body>
+</html>
---
+++ ticks/v12/index.php
@@ -0,0 +1,23 @@
+<?php include ('header.php') ?>
+
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+		<img src="../images/tempcenter.jpg"><br>
+
+
+<?php include ('footer.php') ?>
