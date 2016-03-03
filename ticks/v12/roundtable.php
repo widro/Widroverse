f2b7096b9f5a2e72881d55fc2d1cca9ab6cb92e9
Jonathan Widro <jwidro@gmail.com>
2016-03-03 01:11:07 -0500

    separate roundtable php

---
+++ ticks/v12/roundtable.php
@@ -0,0 +1,59 @@
+<?php include ('header.php') ?>
+
+
+
+
+
+
+
+
+<img src="http://media.insidepulse.com/zones/wrestling/uploads/2014/08/tuesdayraw1600.jpg" alt="" width="650"></div>
+<p>WWE had another new Raw on Monday, what did the Pulse Wrestling crew think of the show?</p>
+<div style="height: 2px; background: #ad151d; width: 100%;"></div>
+<p><img src="http://media.insidepulse.com/shared/images/v7/default120x120_.jpg" alt="" width="120" height="120" align="left"></p>
+<h3>Brittney Soban</h3>
+<p>1. Triple H &amp; Dean Ambrose Start the Show: Ambrose and Triple H did a good job in this segment, reminding fans of the abuse Roman Reigns suffered at the hands of Triple H all while giving Dean the spotlight for another crazy segment of Ambrose ramblings. The fans were behind Ambrose the whole way on this and so was I, allowing Ambrose to be more reckless and off the wall helps his character, all while building his persona up more. A good way to open Raw as Ambrose said he wanted a title shot. Triple H, in classic heel style, said he’d let Ambrose know his answer later and told Ambrose he’d be in a match with Alberto Del Rio later in the night. The moment was a little longer than necessary but not terribly drawn out.</p>
+<p>2. Becky Lynch v. Sasha Banks: This was disappointing from the start of the match to the finish. No time to shine and, with it not given priority ranking in the night, it was a let down all around. WWE can’t keep half-assing the Women’s division if they want us to care.</p>
+<p><a href="http://insidepulse.com/2016/03/01/10-thoughts-on-wwe-monday-night-raw-02-29-2016-undertaker-confront-vince-mcmahon-becky-lynch-v-sasha-banks-albert-del-rio-v-dean-ambrose/">Check out my full 10 Thoughts on Raw!</a></p>
+<p><img src="http://media.insidepulse.com/shared/images/v7/default120x120_.jpg" alt="" width="120" height="120" align="left"></p>
+<h3>JT</h3>
+<p>– Standard Triple H heel promo to start, and as usual for Raw he goes on for waaaay too long. Ambrose comes out and livens things up a bit, but it’s still a hell of a lot of talking.<br>
+– Becky Lynch vs Sasha Banks was an entertaining match, with both women working hard, until the ridiculous double pin finish.<br>
+– The Miz vs Dolph Ziggler was ridiculously short, and I’m beginning to lose patience with this bullshit, Raw.<br>
+– I fast forward for the next hour of TV time and miss absolutely nothing of substance<br>
+– I slow down for the Lucha Dragons vs League of Nations, mainly because my 9 year old son likes luchadores. Half way through the match he asked if we could watch Lucha Underground.<br>
+– Adam Rose vs Ryback was as pointless a match as you ever will see, and while I don’t begrudge any of the Social Outcasts a job, they haven’t done much to interest me so far<br>
+– AJ and Jericho vs New Day was a fun match, and despite some initial reservations I’m starting to like the team of Styles and Jericho.<br>
+– The Undertaker appearance popped the crowd, and apparently he’s white enough to put his hands on Vinnie Mac without getting suspended. Taker leaves the ring and then Vince talks for a few more minutes, because f**k you, viewers.<br>
+– So it’s the Usos vs The Dudleys again, and while it’s not a bad match I am finding it very difficult to care at this point. The same goes for the R Truth / Golddust shenanigans, and just as I’m about to give up on Raw Kevin Owens comes out. Sure, he’s facing the Big Show, but that can’t be that bad, can it? And then the story is that they’re both trying to get a count out victory, which is a great way to make them both look like chumps… FTS.<br>
+– Fast forward to Ambrose vs ADR/ the League, and Dean works hard to put on an interesting match against one of the most boring wrestlers in the company. Utterly predictable finish to an utterly predictable Raw.</p>
+<p><img src="http://media.insidepulse.com/shared/images/v7/default120x120_.jpg" alt="" width="120" height="120" align="left"></p>
+<h3>Darek Foreman</h3>
+<p>Shane McMahon returned last week and turned….NOTHING AROUND!  Shane is strictly an on air talent just like Brock Lesnar and the Rock, so there won’t be a shake up of any kind in the WWE.  That was clear from the opening of the show.</p>
+<p>1. I am going to keep this short and sweet!  Triple H comes out and doesn’t even mention the major shakeup between Vince, Shane and Stephanie.  The COO doesn’t have an opinion on that I guess.  </p>
+<p>2. Ambrose teasing a match with Triple H for the WWE title is intriguing and hints at maybe the matches staying the same, but the Lesnar vs Ambrose becomes the title match.  One can hope right?</p>
+<p>3. Sasha vs Becky ended in a draw….a weird finish.  It’s so obvious that they are going with triple threat, and it has been since the Royal Rumble, so I am not sure why the drawn out tease is going on.</p>
+<p>4. Nothing of note happens until Stephanie comes out for her own 10 min promo.  She makes some valid points, but Shane is still going to be cheered.  Also it is now clear the MAJOR storyline heading into main is the McMahon family drama.</p>
+<p>5. Rey “Kalisto” Jr. can’t seem to breakaway from the feud with the League of “Nationalities”.  He is the US champion, but can’t seem to breakaway from Del Rio and crew or actually defend the title.</p>
+<p>6. Poor Natalya, she can’t get on TV in anything meaningful.  A subway commercial, really? </p>
+<p>7. Ryback is going to try being a heel…again.  Ry-berrg is not getting over, give it up!  The guy is too goofy as a face and despite all of those muscles, something is lacking, and he just doesn’t appear like a monster like Lesnar or even Goldberg.  Social 4MB Job Squad Outcasts are still irrelevant as a group just as they were solo.</p>
+<p>8. Another LONNNNNNGGGG tease with Jericho and AJ Styles playing nice and going after the New Day and their titles in the classic “feuding guys or guys who hate each other” storyline. Hopefully Jericho finally turns on him, but since they have already had 3 matches I have no clue where this feud can even go.</p>
+<p>9. Undertaker returns!…to do what amounted to a run-in during Mr. McMahon’s promo.  No reasons were given on why Undertaker is just going along with this. So for Shane to “take control” of Raw he has to beat the Undertaker? Shane has to beat the Undertaker at WRESTLEMANIA? Anyway, Undertaker said “You know what’s going to happen when I close that door behind me at WrestleMania. You know what I do. The blood of your son is gonna be on your hands, not mine.”  YEAH CAPTAIN OBVIOUS HE BOOKED THE MATCH AND THAT IS WHAT HE WANTS!  HE WON’T FEEL BAD!</p>
+<p>10. Kevin Owens vs the Big Show…NOT FOR THE IC TITLE!  WHY? Both guys traded countdown wins.  WOW!  So maybe this and Jericho and Styles ends at 3/12 network special…right?  </p>
+<p>11. Brie Bella and Lana have words backstage, and Lana comes out after Naomi beats Brie.  Big deal!  Where is Paige? Why is Alicia Fox not hanging with Brie?  Must be a Total Divas storyline or maybe the WWE is throwing crap against the wall to fill  time.</p>
+<p>12. Dean Ambrose vs Alberto Del Rio is a replay of Roman Reigns vs Sheamus from last week, except one was booked properly.  League of Nations magically disappeared last week and left Hunter to beatdown Reigns and play to the crowd with crotch chops and stuff.  This week they got it right by having the Lugs of Nation ordered to attack Ambrose while Triple H looked on.  Classic heel tactic.  Triple H then cleans up whats left.  Easy story, no clue why this same thing wasn’t done last week.</p>
+<p>Final Thoughts…this sets up a title match of Triple H vs Dean Ambrose at the network special.  Will they shakeup the Wrestlemania card on this special?  If not, they have a month to stay in this awkward holding pattern.  And I know a skipped over Bray Wyatt…what was the point…he is the man without a feud heading into Mania.  Outside of the big pop from last week and seeing Shane in the ring, the WWE is not making me very excited about Wrestlemania 32.</p>
+
+
+
+
+				<p>Tags: <a href="http://insidepulse.com/tag/monday-night-raw/" rel="tag">monday night raw</a>, <a href="http://insidepulse.com/tag/raw/" rel="tag">RAW</a>, <a href="http://insidepulse.com/tag/tuesday-raw-roundtable/" rel="tag">Tuesday Raw Roundtable</a>, <a href="http://insidepulse.com/tag/wwe/" rel="tag">WWE</a></p>
+
+
+
+
+
+
+
+
+<?php include ('footer.php') ?>
