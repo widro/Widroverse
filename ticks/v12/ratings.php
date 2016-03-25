49f4988906e6e3180cc43a3067d96896352848d8
Jonathan Widro <jwidro@gmail.com>
2016-03-24 21:53:41 -0400

    wrestling ratings report

--- ticks/v12/ratings.php
+++ ticks/v12/ratings.php
@@ -1,94 +1,144 @@
 <?php include ('header.php') ?>
 
+<img src="http://insidepulse.com/wp-content/uploads/2016/03/wrestlingratingsreportlogo.jpg" alt="wrestlingratingsreportlogo" width="1200" height="800" class="alignnone size-full wp-image-5052133" />
+
+<i>Every week, the Inside Pulse Ratings Report takes a look at the latest TV ratings for all major wrestling shows in the United States. For each show, the latest ratings are analyzed, and compared with the last 5 and 10 week trends. </i>
+
+Hey everyone and welcome to an all new feature at Inside Pulse Wrestling - the weekly Wrestling Ratings Report or the WRR. 
+
+Rather than post ratings news in drips, each week we will collect all the major show TV ratings updates, and compared them with the last few weeks and months for trend analysis. 
+
+With Wrestlemania right around the corner, now is the perfect time to take a look not only at WWE ratings, but all the major shows in the United States wrestling TV landscape.
+
+Let's get to it!
 
 
-Hello and welcome to this week's Wrestling Ratings Report! The WRR is your weekly look at the TV landscape in wrestling, with both in the moment ratings and mutiple week trends.
 
 <img src="http://insidepulse.com/wp-content/uploads/2016/02/WWERaw.png" alt="WWERaw" width="550" height="111" class="alignnone size-full wp-image-5049409" />
 <h2>WWE Monday Night Raw</h2>
-Monday Night Raw
-
-Ratings:
-sdf
+Monday Night Raw continued a downward trend in ratings, averaging 3.4 million viewers and a 1.19 rating in the 18 – 49 demographic for the three hour broadcast. This week's ratings represent the lowest numbers since the January 11th episode which only attracted 3.323 million viewers with a 1.18 demo rating.
 
 
-Last 5 Weeks
+Here is the hourly breakdown:
+8 PM: 1.16 demo rating (3.466 million viewers)
+9 PM: 1.20 demo rating (3.416 million viewers)
+10 PM: 1.20 demo rating (3.317 million viewers)
+
+Clip from this week's show:
+https://www.youtube.com/watch?v=R1JcvUJJhZg
+
+
+Last 10 Weeks
 <ul>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
+<li>March 21, 2016 – 3,399,667 viewers</li>
+<li>March 14, 2016 – 3,457,667 viewers</li>
+<li>March 7, 2016 – 3,551,000 viewers</li>
+<li>February 29, 2016 – 3,724,000 viewers</li>
+<li>February 22, 2016 – 3,884,000 viewers</li>
+<li>February 15, 2016 – 3,460,333 viewers</li>
+<li>February 8, 2016 – 3,726,667 viewers</li>
+<li>February 1, 2016 – 3,383,333 viewers</li>
+<li>January 25, 2016 – 4,098,333 viewers</li>
+<li>January 18, 2016 (Martin Luther King Day) – 3,496,333 viewers</li>
+
 </ul>
+
 
 <img src="http://insidepulse.com/wp-content/uploads/2016/02/smackdown.png" alt="smackdown" width="599" height="334" class="alignnone size-full wp-image-5049408" />
 <h2>WWE Thursday Night Smackdown</h2>
-sdfsdfsdf
-
-Ratings:
-sdf
+This week’s episode of WWE Thursday Night Smackdown on the USA Network did 2,260,000 viewers. That’s up 74,000 viewers from the week prior and that is actually significant, because there was a lot of competition with the 2016 NCAA Men’s Basketball Tournament as opposition.
 
 
-Last 5 Weeks
+Clip from this week's show:
+Top 10 SmackDown moments: WWE Top 10, March 17, 2016
+https://www.youtube.com/watch?v=XcOo8YbIYh8
+
+Last 10 Weeks:
 <ul>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
+<li>March 17, 2016 – 2,260,000 viewers</li>
+<li>March 10, 2016 – 2,186,000 viewers</li>
+<li>March 3, 2016 – 2,491,000 viewers</li>
+<li>February 25, 2016 – 2,395,000 viewers</li>
+<li>February 18, 2016 – 2,458,000 viewers</li>
+<li>February 11, 2016 – 2,414,000 viewers</li>
+<li>February 4, 2016 – 2,664,000 viewers</li>
+<li>January 28, 2016 – 2,720,000 viewers</li>
+<li>January 21, 2016 – 2,757,000 viewers</li>
+<li>January 14, 2016 – 2,332,000 viewers</li>
 </ul>
 
 
 <img src="http://insidepulse.com/wp-content/uploads/2016/02/Impact-Logo-500.png" alt="Impact-Logo-500" width="500" height="163" class="alignnone size-full wp-image-5049406" />
 <h2>TNA Tuesday Night Impact</h2>
-sdfsdfsdf
-
-Ratings:
-sdf
+This week's episode of TNA Impact on PopTV rose again from last week's live show, attracting 331,000 viewers and a 0.1 rating in the 18 – 49 demographic. 
 
 
-Last 5 Weeks
+Clip from this week's show:
+https://www.youtube.com/watch?v=s9EU5IG_ti0
+
+Last 10 Weeks
 <ul>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
+<li>March 22, 2016 - 331,000 viewers</li>
+<li>March 15, 2016 - 307,000 viewers</li>
+<li>March 8, 2016 - 222,000 viewers</li>
+<li>March 1, 2016 - 258,000 viewers</li>
+<li>February 23, 2016 - 210,000 viewers</li>
+<li>February 16, 2016 - 273,000 viewers</li>
+<li>February 9, 2016 - 276,000 viewers</li>
+<li>February 2, 2016 - 295,000 viewers</li>
+<li>January 26, 2016 - 339,000 viewers</li>
+<li>January 19, 2016 - 321,000 viewers</li>
+<li>January 12, 2016 - 288,000 viewers</li>
+<li>January 5, 2016 - 255,000 viewers</li>
 </ul>
 
 
 <img src="http://insidepulse.com/wp-content/uploads/2016/02/lucha-underground-splash.jpg" alt="lucha-underground-splash" width="303" height="201" class="alignnone size-full wp-image-5049407" />
 <h2>Lucha Underground Season 2</h2>
-sdfsdfsdf
-
 Ratings:
-sdf
+Lucha Underground saw an huge jump in viewership numbers and ratings for this week's new episode. The March 23rd, 2016 episode brought in 163,000 viewers and a 0.06 in the 18 – 49 demographic, up 63% and double from last week’s 100,000 viewers and 0.03 demo rating. The viewership is a new series high, topping the 152,000 viewers that the February 17th episode pulled in. The rating tied that week’s demo number.
 
+Lucha Underground also ranked among the night’s top 150 cable originals for the first time, coming in at #137.
 
-Last 5 Weeks
+Clip from this week's show:
+https://www.youtube.com/watch?v=ajEwv4YwjPo
+
+Last 10 Weeks
 <ul>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
+<li>March 23, 2016 - 163,000 viewers</li>
+<li>March 16, 2016 - 100,000 viewers</li>
+<li>March 9, 2016 - 95,000  viewers</li>
+<li>March 2, 2016 - 124,000 viewers</li>
+<li>February 24, 2016 - 99,000 viewers</li>
+<li>February 17, 2016 - 152,000 viewers</li>
+<li>February 10, 2016 - 136,000 viewers</li>
+<li>February 3, 2016 - ,000 viewers</li>
+<li>January 27, 2016 - 44,000 viewers</li>
+<li>January 20, 2016 - ,000 viewers</li>
+<li>January 13, 2016 - ,000 viewers</li>
+<li>January 6, 2016 - ,000 viewers</li>
 </ul>
 
 
-<h2>Total Divas</h2>
-sdfsdfsdf
-
-Ratings:
-sdf
+<img src="http://insidepulse.com/wp-content/uploads/2015/07/totaldivas2015season4.png" alt="totaldivas2015season4" width="650" height="325" class="alignnone size-full wp-image-5028079" /><h2>Total Divas</h2>
+Total Divas saw a minor uptick in viewership this week while ratings stayed even. Last night’s episode brought in 663,000 viewers and a 0.31 rating in the 18 – 49 demographic, up 2% and even respectively from 650,000 and a 0.31 demo rating last week. The viewership is the best for the show since the Febuary 23th episode brought in 777,000 viewers.
 
 
-Last 5 Weeks
+Clip from this week's show:
+https://www.youtube.com/watch?v=z0b87IY-RsQ
+
+Last 10 Weeks
 <ul>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
-<li></li>
+<li>March 22, 2016 - 663,000 viewers</li>
+<li>March 15, 2016 - 650,000 viewers</li>
+<li>March 8, 2016 - 580,000 viewers</li>
+<li>March 1, 2016 - 591,000 viewers</li>
+<li>February 23, 2016 - 777,000 viewers</li>
+<li>February 16, 2016 - 808,000 viewers</li>
+<li>February 9, 2016 - 810,000 viewers</li>
+<li>February 2, 2016 - 840,000 viewers</li>
+<li>January 26, 2016 - 939,000 viewers</li>
+<li>January 19, 2016 - 1.115 million viewers</li>
 </ul>
 
 
